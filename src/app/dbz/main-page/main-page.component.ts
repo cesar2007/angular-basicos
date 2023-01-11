8b8d643c801e570c0e07aa4726bf09fcb9d5a049
CESAR MIRANDA <cesar.miranda@oreillyauto.com>
2023-01-11 16:20:56 -0600

    dbz añadido

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent  {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 8500
+    }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+    
+  }
+
+  cambiarNombre(event:any){
+    console.log(event.target.value)
+  }
+
+  agregar(){
+    if (this.nuevo.nombre.trim().length === 0){return;}
+
+    console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
+
+}
+                            

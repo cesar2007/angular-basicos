e5d27f5719e442943ea07fae151febfe12b0c6b0
CESAR MIRANDA <cesar.miranda@oreillyauto.com>
2023-01-12 14:48:48 -0600

    actualzacion sin mensajes

--- src/app/dbz/interfaces/dbz.interface.ts
+++ src/app/dbz/interfaces/dbz.interface.ts
@@ -1,11 +1,3 @@
-// Este es un comentario de prueba para la gestión de git
-
-// este es otro cambio para git
-
-// cambio 3 en git
-
-
-
 export interface Personaje {
     nombre: string;
     poder: number;
